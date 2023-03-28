6667a02f7c62b0b665c45ad5704ca4c1faa37596
Anton Krasnyuk <a.krasniuk@stdp.ru>
2023-03-29 01:54:15 +0300

    arrays change

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,21 @@
 function compareArrays(arr1, arr2) {
-  
+  return arr1.every((el, i) => el === arr2[i] && arr1.length === arr2.length);
 }
 
+module.exports = {
+  compareArrays,
+};
+
 function getUsersNamesInAgeRange(users, gender) {
-  
+  if (users.length === 0) return 0;
+  const genderArray = users.filter((user) => user.gender === gender);
+  if (genderArray.length === 0) return 0;
+  const res = genderArray.reduce((acc, user) => {
+    return acc + user.age;
+  }, 0);
+  return res / genderArray.length;
 }
+
+module.exports = {
+  getUsersNamesInAgeRange,
+};
