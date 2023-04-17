04da8e03dfabdb1e6d5106b79c8acd4d4c2392d8
Anton Krasnyuk <a.krasniuk@stdp.ru>
2023-04-17 12:15:20 +0300

    asunc change

---
+++ 7.async/task.js
@@ -0,0 +1,76 @@
+class AlarmClock {
+  constructor() {
+    this.alarmCollection = [];
+    this.intervalId = null;
+  }
+
+  addClock = (timeStart, alarmPlay) => {
+    if (!timeStart || !alarmPlay) {
+      throw new Error("Отсутствуют обязательные аргументы");
+    }
+
+    if (this.alarmCollection.includes(timeStart)) {
+      console.warn("Уже присутствует звонок на это же время");
+      return;
+    }
+
+    const alarmItem = {
+      callback: alarmPlay,
+      time: timeStart,
+      canCall: true,
+    };
+
+    this.alarmCollection.push(alarmItem);
+  };
+
+  removeClock = (time) => {
+    this.alarmCollection = this.alarmCollection.filter(
+      (item) => item.time !== time
+    );
+  };
+
+  getCurrentFormattedTime = () => {
+    const nowTime = new Date();
+
+    const nowTimeFormated = `${
+      nowTime.getHours() < 9 ? `0${nowTime.getHours()}` : nowTime.getHours()
+    }:${
+      nowTime.getMinutes() < 9
+        ? `0${nowTime.getMinutes()}`
+        : nowTime.getMinutes()
+    }`;
+
+    return nowTimeFormated;
+  };
+
+  start = () => {
+    if (this.intervalId) {
+      return;
+    }
+
+    this.intervalId = setInterval(() => {
+      this.alarmCollection.forEach((item) => {
+        if (item.time === this.getCurrentFormattedTime() && item.canCall) {
+          item.canCall = false;
+          item.callback();
+        }
+      });
+    }, 1000);
+  };
+
+  stop = () => {
+    clearInterval(this.intervalId);
+    this.intervalId = null;
+  };
+
+  resetAllCalls = () => {
+    this.alarmCollection.forEach((item) => {
+      item.canCall = true;
+    });
+  };
+
+  clearAlarms = () => {
+    this.stop();
+    this.alarmCollection = [];
+  };
+}
